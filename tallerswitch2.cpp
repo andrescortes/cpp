a5baaf740ac6c4e65d927050033a11fb807d838d
Andres Cortes <andresabyac@gmail.com>
2018-08-24 12:04:44 -0700

    taller swich ejercicio 2

--- tallerswitch2.cpp
+++ tallerswitch2.cpp
@@ -1,28 +1,24 @@
-/*2-. Que pida un número del 1 al 12 y diga el nombre del mes correspondiente.
-*/
+/*2-. 3-. Que pida una letra y detecte si es una vocal, 
+en caso afirmativo imprimir una palabra que empiece por esa vocal.*/
 #include <iostream>
 #include <conio.h>
 using namespace std;
 main(){
- int dia;
- cout << "Ingrese un numero para decirle a que mes corresponde " << endl;
- cin >> dia;
- 
- switch(dia){
- 	case 1: cout << "Mes Enero." ; break;
- 	case 2: cout << "Mes Febrero." ; break;
- 	case 3: cout << "Mes Marzo." ; break;
- 	case 4: cout << "Mes Abril." ; break;
- 	case 5: cout << "Mes Mayo." ; break;
- 	case 6: cout << "Mes Junio." ; break;
- 	case 7: cout << "Mes Julio." ; break;
- 	case 8: cout << "Mes Agosto." ; break;
- 	case 9: cout << "Mes Septiembre." ; break;
- 	case 10: cout << "Mes Octubre." ; break;
- 	case 11: cout << "Mes Noviembre." ; break;
- 	case 12: cout << "Mes Diciembre." ; break;
- 	default: cout << "Numero no valido o Caracter no valido.";
- }	
-
+ char letra;
+ cout << " Ingrese una letra: " << endl;
+ cin >> letra;
+ if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u'){
+ switch(letra){
+ 	case 'a': cout  << " a de alfabeto" ; break;
+ 	case 'e': cout << " e de elefane " ; break;
+ 	case 'i': cout << " i de iglesia" ; break;
+ 	case 'o': cout << " o de oreja" ; break;
+ 	case 'u': cout << " u de unguento" ; break;
+     }	
+  }
+  else {
+  	cout << "¡ Caracter no valido !";
+  }
+  getch();
 }
 
