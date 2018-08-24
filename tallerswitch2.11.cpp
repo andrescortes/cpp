d283d3131ba669c97cfc310880e66e61057c9e3a
Andres Cortes <andresabyac@gmail.com>
2018-08-24 12:39:10 -0700

    switch ejer 2

---
+++ tallerswitch2.11.cpp
@@ -0,0 +1,26 @@
+/*2-. Que pida un número del 1 al 12 y diga el nombre del mes correspondiente.*/
+#include <iostream>
+#include <conio.h>
+using namespace std;
+main(){
+ int dia;
+ cout << "Ingrese un numero para decirle a que mes corresponde: " << endl;
+ cin >> dia;
+ 
+ switch(dia){
+ 	case 1: cout << "enero." ; break;
+ 	case 2: cout << "febrero" ; break;
+ 	case 3: cout << "marzo." ; break;
+ 	case 4: cout << "abril" ; break;
+ 	case 5: cout << "mayo" ; break;
+ 	case 6: cout << "junio" ; break;
+ 	case 7: cout << "julio" ; break;
+ 	case 8: cout << "agosto" ; break;
+ 	case 9: cout << "septiembre" ; break;
+ 	case 10: cout << "octubre" ; break;
+ 	case 11: cout << "noviembre" ; break;
+ 	case 12: cout << "diciembre" ; break;
+ 	default: cout << "Numero no valido o Caracter no valido.";
+ }	
+
+}
